104b71eec02b7ea195d923fa7493ed244f751932
Lucian <lucianmurmurache@gmail.com>
2024-02-11 20:52:23 +0100

    chore: creates footer navbar and loading components

---
+++ src/components/Loading.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="flex items-center justify-center">
+            <div
+                class="spinner-border inline-block h-8 w-8 animate-spin rounded-full border-4"
+                role="status"
+            >
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+.spinner-border {
+    border-color: transparent;
+    border-top-color: #60993e;
+    border-right-color: #60993e;
+}
+.visually-hidden {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+    white-space: nowrap;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav
+        class="bg-darkBrown/80 fixed left-0 right-0 top-0 z-10 flex justify-end px-5 py-2 backdrop-blur md:px-10"
+    >
+        <div class="mr-auto text-4xl">
+            <router-link to="/">
+                <span
+                    role="img"
+                    alt="Navigate to Homepage"
+                    title="Navigate to Homepage"
+                >
+                    📚
+                </span>
+            </router-link>
+        </div>
+        <div v-if="authStore.token" class="flex flex-row items-center gap-x-5">
+            <span v-if="username" class="text-lg capitalize">
+                {{ username }}</span
+            >
+            <button @click="logout" class="button-secondary">Logout</button>
+        </div>
+        <div v-else class="flex flex-row items-center gap-x-5 py-3">
+            <router-link to="/login">
+                <span class="button-secondary">Login</span>
+            </router-link>
+            <router-link to="/register">
+                <span class="button-secondary">Register</span>
+            </router-link>
+        </div>
+    </nav>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useAuthStore } from '@/store/auth';
+const authStore = useAuthStore();
+const username = computed(() => authStore.user);
+
+function logout() {
+    authStore.logout();
+}
+</script>
