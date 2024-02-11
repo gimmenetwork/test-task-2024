710779bda2c69981fdc3bf44a7b362bc3a86ebcc
Lucian <lucianmurmurache@gmail.com>
2024-02-11 18:21:25 +0100

    chore: creates booksview and addnewbook - WIP

---
+++ src/views/books/AddNewBook.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Add Book View</h1>
+        <!-- TODO: Create functionality for adding a book -->
+    </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/views/books/BooksView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Books View</h1>
+        <!-- TODO: Add books from store -->
+        <router-link to="/add-book">Add Book</router-link>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
