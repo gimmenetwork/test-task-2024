2c43ea0b3dc0c9c3c84ff5c9bc76477a6423a7ac
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-11 11:51:29 +0100

    add custom tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'bermuda-gray': {
+        '50': '#f5f8f9',
+        '100': '#e7eff2',
+        '200': '#d5e2e8',
+        '300': '#b8cfd8',
+        '400': '#95b7c5',
+        '500': '#7ba1b6',
+        '600': '#688ba6',
+        '700': '#5d7c98',
+        '800': '#50667d',
+        '900': '#435565',
+        '950': '#2c353f',
+    },
+    
+    }
   },
   plugins: [],
 }
