6228b3e72df07807aa3d075ab3904d1068a0927e
Lucian <lucianmurmurache@gmail.com>
2024-02-11 20:50:58 +0100

    chore: updates not found view content

--- src/views/general/PageNotFound.vue
+++ src/views/general/PageNotFound.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        <h1>Page not found</h1>
+    <div class="grid place-items-center gap-y-10">
+        <h1 class="text-3xl md:text-5xl">Page not found 🧭🚶</h1>
+        <div class="text-center">
+            <p>Opps! You navigated to a non-existent page...</p>
+            <br />
+            <p class="inline">Try going back to the previous page or&nbsp;</p>
+            <router-link to="/"
+                ><span class="link">click here</span>
+            </router-link>
+            <p class="inline">&nbsp;to go to the homepage</p>
+        </div>
     </div>
 </template>
 <script setup lang="ts"></script>
