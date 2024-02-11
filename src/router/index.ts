0a4a07c3585d8925e097dbdd5c77431fdce42bf5
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-11 17:09:19 +0100

    update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
+import LoginView from '@/views/LoginView.vue'
+import HomeView from '@/views/HomeView.vue'
+import { isAuthenticated } from "@/helpers/authHelper"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,13 +9,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/LoginView.vue')
       component: LoginView
     },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import('@/views/RegisterView.vue')
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+      beforeEnter(){
+        if (!isAuthenticated()) router.push('/')
+      }
+    }
   ]
 })
+
 
 export default router
