b6dce3d82af5785eebf3fbe5a787f15ce7f4c98c
Lucian <lucianmurmurache@gmail.com>
2024-02-11 20:49:39 +0100

    chore: updates auth content

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,30 +1,56 @@
 <template>
-    <form @submit.prevent="registerAndLogin" class="flex flex-col gap-y-2.5">
-        <input required type="text" v-model="username" placeholder="Username" />
-        <div class="relative">
-            <p v-if="usernameError" class="absolute -top-6 text-red-500">
-                {{ usernameError }}
-            </p>
+    <div>
+        <h1 class="mb-5 text-center font-semibold">PageTracker 📚</h1>
+        <div class="form-wrapper">
+            <h2 class="mb-5 text-center text-2xl font-semibold">Register</h2>
+            <form
+                @submit.prevent="registerAndLogin"
+                class="flex flex-col gap-y-2.5"
+            >
+                <input
+                    required
+                    type="text"
+                    class="input"
+                    v-model="username"
+                    placeholder="Pick a username"
+                />
+                <div class="relative">
+                    <p
+                        v-if="usernameError"
+                        class="absolute -top-6 text-red-500"
+                    >
+                        {{ usernameError }}
+                    </p>
+                </div>
+
+                <input
+                    required
+                    class="input"
+                    type="password"
+                    v-model="password"
+                    placeholder="Choose a password"
+                />
+                <div class="relative">
+                    <p
+                        v-if="passwordError"
+                        class="absolute -top-6 text-red-500"
+                    >
+                        {{ passwordError }}
+                    </p>
+                </div>
+
+                <button type="submit" class="button-primary mb-2.5">
+                    Register
+                </button>
+                <div class="flex flex-col text-center">
+                    <p>Already have an account?</p>
+                    <router-link to="/login">
+                        <span class="link">Login</span>
+                    </router-link>
+                </div>
+            </form>
         </div>
-
-        <input
-            required
-            type="password"
-            v-model="password"
-            placeholder="Password"
-        />
-        <div class="relative">
-            <p v-if="passwordError" class="absolute -top-6 text-red-500">
-                {{ passwordError }}
-            </p>
-        </div>
-
-        <button type="submit" class="button-primary mb-2.5">Register</button>
-
-        <router-link to="/login">
-            <span class="link">Login</span>
-        </router-link>
-    </form>
+    </div>
 </template>
 
 <script lang="ts">
@@ -67,4 +93,3 @@ export default defineComponent({
     },
 });
 </script>
-@/stores/auth
