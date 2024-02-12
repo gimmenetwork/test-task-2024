6f4983b21f78e8ac2020ff520384449bb4040236
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-12 22:55:22 +0100

    add book interface

---
+++ src/types/Books/Book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    id:string
+    userId: string
+    title: string
+    genre: string
+    pageCount: string
+    pagesRead: string
+    reviews: string
+    rating: string
+}
