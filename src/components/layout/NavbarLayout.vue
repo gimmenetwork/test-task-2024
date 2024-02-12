40f28217f04d97d30bfbf11dd3be097a3d2f0fca
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-12 22:53:27 +0100

    update logout function

--- src/components/layout/NavbarLayout.vue
+++ src/components/layout/NavbarLayout.vue
@@ -6,20 +6,17 @@ const router = useRouter()
 const { authLogout } = useAuthStore()
 
 function logout(){
-  const loggedOut = authLogout()
-  if(loggedOut){
-    router.push('/')
-  }
- 
+  authLogout()
+  router.push('/')
 }
 </script>
 
 <template>
   <nav class="bg-bermuda-gray-200">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a class="flex items-center space-x-3 rtl:space-x-reverse text-white">
+    <a class="flex items-center space-x-3 rtl:space-x-reverse text-bermuda-gray-900 font-bold">
       <!-- logo -->
-      Book-club-service
+      Book-Club-Service
     </a>
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -30,7 +27,7 @@ function logout(){
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
         <li>
-          <button class="text-white" @click="logout">Logout</button>
+          <button class="text-bermuda-gray-800 px-4 py-2" @click="logout">Logout</button>
         </li>
       </ul>
     </div>
@@ -38,3 +35,4 @@ function logout(){
 </nav>
 
 </template>
+@/store/authStore@/stores/authStore
