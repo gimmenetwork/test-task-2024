d84b3df9899291d44393bb9f6aa8c17fbcbd9abc
Lucian <lucianmurmurache@gmail.com>
2024-02-12 12:54:44 +0100

    style: makes review button secondary

--- src/views/books/BooksView.vue
+++ src/views/books/BooksView.vue
@@ -54,7 +54,7 @@
                     <button
                         v-if="book.progress === 100"
                         @click="goToReviewPage(book.id)"
-                        class="button-primary"
+                        class="button-secondary"
                     >
                         <span v-if="book.review?.text">Edit Review</span>
                         <span v-else> Write a Review </span>
