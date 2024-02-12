2d6abb9de4b05c5794bb605b9ca02e493504532d
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-12 22:55:41 +0100

    Add book view component to Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import NavbarLayout from '@/components/layout/NavbarLayout.vue'
+import BookView from '@/components/BookView.vue'
 
 </script>
 
 <template>
   <NavbarLayout />
-  <main>Home</main>
-  <Login />
+  <BookView />
 </template>
