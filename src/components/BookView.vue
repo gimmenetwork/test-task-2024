83d383fcebb36ebc184bff319add44550af2b5a1
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-13 01:51:43 +0100

    add new stats

--- src/components/BookView.vue
+++ src/components/BookView.vue
@@ -21,7 +21,7 @@ function toggleAddBook(){
       <section class="w-full px-12 py-12 bg-blue shadow">
         <header>
           <section class="flex flex-row justify-between py-2">
-              <h1 class="text-xl font-semibold leading-tight text-gray-900 md:text-2xl">My Books</h1>
+              <h1 class="text-xl font-semibold leading-tight text-gray-900 md:text-2xl">My Book Tracker</h1>
               <button class="w-30 bg-green-400 px-10 py-2 rounded-xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium me-2 mb-2 focus:outline-none" @click="toggleAddBook" v-if="!showAddNewBookView">Add new book</button>
               <button class="w-30 bg-green-400 px-5 py-2 rounded-xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium me-2 mb-2 focus:outline-none" @click="toggleAddBook" v-if="showAddNewBookView">X</button>
           </section>
@@ -31,8 +31,18 @@ function toggleAddBook(){
         </section>
         
         <section>
-        <h1 class="text-xl leading-tight font-semibold text-bermuda-gray-700 md:text-xl py-2">All Book List</h1>
-        <p class="text-red-400" v-if="bookStore.Books.length <= 0">No books added yet!</p>
+       
+        <div>
+          <p class="py-2 font-semilight text-bermuda-gray-700">Highest number of Pages read: <span class="font-light">{{ bookStore.totalPagesRead }}</span></p>
+          <p class="py-2 font-semilight text-bermuda-gray-700">Popular genres you are reading: </p>
+          <div v-for="genre in bookStore.popularGenres" :key="genre" class="font-semibold text-bermuda-gray-700">
+            <p class="italic font-light">{{ genre }}</p>
+          </div>
+        </div>
+        <div class="flex flex-row justify-center">
+          <h1 class="text-2xl leading-tight font-semibold text-bermuda-gray-700 md:text-2xl py-4">My Book List</h1>
+          <p class="text-red-400" v-if="bookStore.Books.length <= 0">No books added yet!</p>
+        </div>
         <Suspense>
           <BookList />
           <template #fallback>
@@ -42,7 +52,9 @@ function toggleAddBook(){
         </section>
         <section>
           <h1 class="text-2xl font-semibold leading-tight text-bermuda-gray-700 md:text-xl">Completed Books</h1>
-          <p>You currently do not have any completed books!</p>
+          <div v-for="book in bookStore.completedBooks" :key="book.id" class="font-semibold text-bermuda-gray-700">
+            <p class="italic font-light">{{ book.title }}</p>
+          </div>
         </section>
 
       </section>
