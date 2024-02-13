66f8c110ef2a446855007688c5b7adb8511865ef
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-13 12:18:27 +0100

    add test mock data

---
+++ src/components/__tests__/mocks/mockGetUserBooks.ts
@@ -0,0 +1,44 @@
+const getAllUsersBooksResponse = [
+  {
+    id: '3cd90',
+    userId: '67f8997g87g8665554',
+    title: 'Lies To Love',
+    genre: 'Romance',
+    pageCount: '323',
+    pagesRead: '70',
+    reviews: '',
+    rating: ''
+  },
+  {
+    id: '28cgj',
+    userId: '67f8997g87g8665554',
+    title: 'Witch Hunt',
+    genre: 'Fantasy',
+    pageCount: '22',
+    pagesRead: '22',
+    reviews: '',
+    rating: ''
+  },
+  {
+    id: '4ff94',
+    userId: '67f8997g87g8665554',
+    title: 'Rich Dad Poor Dad',
+    genre: 'Business',
+    pageCount: '200',
+    pagesRead: '105',
+    reviews: 'An awesome read about money',
+    rating: ''
+  },
+  {
+    id: '',
+    title: 'One Time',
+    genre: 'Romance',
+    pageCount: '561',
+    userId: '67f8997g87g8665554',
+    pagesRead: '561',
+    reviews: '',
+    rating: ''
+  }
+]
+
+export default getAllUsersBooksResponse
