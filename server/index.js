77a6dec1ce34c237c575c49acabb407108e468aa
Jaidev Singh Malik <jaidevsinghmalik@jaidevs-air.speedport.ip>
2024-02-13 13:48:17 +0100

    backend completed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 // Middlewares
@@ -28,6 +28,60 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // create a collection of documents
+    const bookCollections = client.db("BookInventory").collection("books");
+
+    // insert a book to the db : using post method
+    app.post("/upload-book", async (req, res) => {
+        const data = req.body;
+        const result = await bookCollections.insertOne(data);
+        res.send(result);
+    });
+
+    // get all books from the db : using get method
+    app.get("/all-books", async (req, res) => {
+      const books = await bookCollections.find();
+      const result = await books.toArray();
+      res.send(result);
+    })
+
+    // update a book in the db : using patch method
+    app.patch("/book/:id", async(req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const updateBookData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set : {
+          ...updateBookData
+        }
+      }
+      // update the book
+      const result = await bookCollections.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+
+    // delete a book from the db : using delete method
+    app.delete("/book/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.deleteOne(filter);
+      res.send(result);
+    })
+
+    // find by category
+    app.get("/all-books", async(req, res) => {
+      let query = {};
+      if(req.query?.category) {
+        query = { category: req.query.category };
+      }
+      const result = await bookCollections.find(query).toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
