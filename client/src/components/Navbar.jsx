11ad2218759e3d42f507f8a350f5303e01a02be5
Jaidev Singh Malik <jaidevsinghmalik@jaidevs-air.speedport.ip>
2024-02-13 16:47:52 +0100

    navbar completed working on homepage now

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaBlog } from "react-icons/fa6"
+import { FaBarsStaggered, FaBlog, FaXmark } from "react-icons/fa6"
 
 const Navbar = () => {
 
@@ -50,24 +50,48 @@ const Navbar = () => {
         }
     ]
 
-  return (
-    <header>
-        <nav>
-            <div>
-                {/*LOGO*/}
-                <Link to="/" className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className='inline-block'/>Books</Link>
+    return (
+        <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
+            <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300}" : ""}`}>
+                <div className='flex justify-between items-center text-base gap-8'>
+                    {/*LOGO*/}
+                    <Link to="/" className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className='inline-block' />Books</Link>
 
-                {/* nav items for large devices */}
+                    {/* nav items for large devices */}
 
-                <ul>
-                    {navItems.map(({link, path}) => (
-                        <Link key = {path} to={path} className='block text-base text-black'>{link}</Link>
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    </header>
-  )
+                    <ul className='md:flex space-x-12 hidden'>
+                        {
+                            navItems.map(({ link, path }) =>
+                                <Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>{link}</Link>
+                            )}
+                    </ul>
+
+                    {/* button for large devices */}
+                    <div className='space-x-12 hidden lg:flex items-center'>
+                        <button>
+                            <FaBarsStaggered className='w-5 hover:text-blue-700' />
+                        </button>
+                    </div>
+
+                    {/* menu button for mobile devices */}
+                    <div className='md:hidden'>
+                        <button onClick={toggleMenu} className='text-black focus:outline-none'>
+                            isMenuOpen ? <FaXmark className='h-5 w-5 text-black' /> : <FaBarsStaggered className='h-5 w-5 text-black' />
+                        </button>
+                    </div>
+
+                    {/* nav items for small devices */}
+                    <div className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+                        {
+                            navItems.map(({ link, path }) => (
+                                <Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer'>{link}</Link>
+                            ))
+                        }
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
