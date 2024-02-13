baa87f1a082f8f27b206fcd8e3c20ff7705ee2f5
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2024-02-13 12:15:37 +0100

    add global test setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  test: {
+    globals: true,
+    setupFiles: 'src/components/__tests__/setup/setupTests.spec.ts'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
