e09fea9dc583976f38c9049260c0933a7218c194
Jaidev Singh Malik <jaidevsinghmalik@jaidevs-air.speedport.ip>
2024-02-14 16:55:29 +0100

    shop completed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,12 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
     // 'node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
   },
   // plugins: [require('flowbite/plugin')],
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
