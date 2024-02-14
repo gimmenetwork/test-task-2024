0387d7fc9d528a49b59b9264f94e67271cd40940
Jaidev Singh Malik <jaidevsinghmalik@jaidevs-air.speedport.ip>
2024-02-14 21:20:23 +0100

    Upload book form created

--- client/src/dashboard/UploadBook.jsx
+++ client/src/dashboard/UploadBook.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Button, Checkbox, Label, Select, TextInput, Textarea } from 'flowbite-react';
 
 const UploadBook = () => {
+  const bookCategories = [
+    "Fiction",
+    "Non-Fiction",
+    "Science",
+    "Technology",
+    "Biography",
+    "Business",
+    "Self-Help",
+    "Health",
+    "Fitness",
+    "History",
+    "Travel",
+    "Cooking",
+    "Art",
+    "Photography",
+    "Religion",
+    "Philosophy",
+    "Sports"
+  ]
+
+  const [selectedBookCategories, setSelectedBookCategories] = useState(bookCategories[0])
+
+  const handleChangeSelectedValue = (event) => {
+    // console.log(event.target.value)
+    setSelectedBookCategories(event.target.value)
+  }
+
   return (
     <div className='px-4 my-12'>
       <h2 className='mb-8 text-3xl font-bold'>Upload A Book</h2>
+      <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+        {/* First Row */}
+        <div className='flex gap-8'>
+          {/* bookTitle */}
+          <div className='lg:w-1/2'>
+            <div className="mb-2 block">
+              <Label htmlFor="bookTitle" value="Book Title" />
+            </div>
+            <TextInput id="bookTitle" type="text" placeholder="Book Name" name='bookTitle' required />
+          </div>
+          {/* authorName */}
+          <div className='lg:w-1/2'>
+            <div className="mb-2 block">
+              <Label htmlFor="authorName" value="Author Name" />
+            </div>
+            <TextInput id="authorName" type="text" placeholder="Author Name" name='authorName' required />
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className='flex gap-8'>
+          {/* imageURL */}
+          <div className='lg:w-1/2'>
+            <div className="mb-2 block">
+              <Label htmlFor="imageURL" value="Book Image URL" />
+            </div>
+            <TextInput id="imageURL" type="text" placeholder="Book Image URL" name='imageURL' required />
+          </div>
+          {/* category */}
+          <div className='lg:w-1/2'>
+            <div className="mb-2 block">
+              <Label htmlFor="inputState" value="Book Category" />
+            </div>
+            <Select id="inputState" name="categoryName" value={selectedBookCategories} onChange={handleChangeSelectedValue} className='w-full rounded'>
+              {
+                bookCategories.map((option) => <option key={option} value={option}>{option}</option>)
+              }
+            </Select>
+          </div>
+        </div>
+
+        {/* bookDescription */}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookDescription" value="Book Description" />
+          </div>
+          <Textarea id="bookDescription" type="text" placeholder="Book Description" name='bookDescription' rows={4} required className='w-full rounded' />
+        </div>
+
+        {/* book pdf url */}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookPDFURL" value="Book PDF URL" />
+          </div>
+          <Textarea id="bookPDFURL" type="text" placeholder="Book PDF URL" name='bookPDFURL' required className='w-full rounded' />
+        </div>
+
+        <Button type='submit' className='mt-5'>Upload Book</Button>
+
+      </form>
     </div>
   )
 }
