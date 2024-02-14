d610910367ebe5dd832be35e8457355556bc2039
Jaidev Singh Malik <jaidevsinghmalik@jaidevs-air.speedport.ip>
2024-02-14 16:37:25 +0100

    footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './home/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className='min-h-screen'>
+        <Outlet />
+      </div>
+      <Footer />
     </>
   )
 }
