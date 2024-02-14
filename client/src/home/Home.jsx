e4507ccd9824fe5c7cfbaf35704865b8d5c03c1a
Jaidev Singh Malik <jaidevsinghmalik@jaidevs-air.speedport.ip>
2024-02-14 15:53:09 +0100

    new feature added of promo code

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/Banner'
 import BestSellerBooks from './BestSellerBooks'
 import FavBook from './FavBook'
+import PromoBanner from './PromoBanner'
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
       <Banner />
       <BestSellerBooks />
       <FavBook />
+      <PromoBanner />
     </div>
   )
 }
