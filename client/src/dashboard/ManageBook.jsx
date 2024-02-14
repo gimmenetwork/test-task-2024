89ef1e3c487c9a9cdf84a9b9438c17cebf65c677
Jaidev Singh Malik <jaidevsinghmalik@Jaidevs-MacBook-Air.local>
2024-02-14 23:45:07 +0100

    Manage book component completed and integerated with the server

--- client/src/dashboard/ManageBook.jsx
+++ client/src/dashboard/ManageBook.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 const ManageBook = () => {
+  const [allBooks, setAllBooks] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/all-books')
+      .then(res => res.json())
+      .then(data => setAllBooks(data))
+  }, [])
+
+  // Delete book
+  const handleDelete = (id) => {
+    console.log(id)
+    fetch(`http://localhost:3000/book/${id}`, {
+      method: 'DELETE'
+    }).then(res => res.json()).then(data => {
+      alert("Book is deleted successfully!")
+      // setAllBooks(data)
+    })
+  }
   return (
-    <div>ManageBook</div>
+    <div className='px-4 my-12'>
+      <h2 className='mb-8 text-3xl font-bold'>Manage Your Books</h2>
+
+      {/* Table for book data */}
+      <Table className='lg:w-[1180px]'>
+        <Table.Head>
+          <Table.HeadCell>Serial No.</Table.HeadCell>
+          <Table.HeadCell>Book Name</Table.HeadCell>
+          <Table.HeadCell>Author Name</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>
+            <span className="">Edit or Manage</span>
+          </Table.HeadCell>
+        </Table.Head>
+        {
+          allBooks.map((book, index) => <Table.Body className='divide-y' key={book._id}>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {index + 1}
+            </Table.Cell>
+            <Table.Cell>{book.bookTitle}</Table.Cell>
+            <Table.Cell>{book.authorName}</Table.Cell>
+            <Table.Cell>{book.category}</Table.Cell>
+            <Table.Cell>$10.00</Table.Cell>
+            <Table.Cell>
+              <Link to={`/admin/dashboard/ediit-books/${book._id}`} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5">
+                Edit
+              </Link>
+              <button onClick={() => handleDelete(book._id)} className='bg-red-600 px-4 py-1 font-semibold text-white rounded-sm hover:bg-sky-600'>Delete</button>
+            </Table.Cell>
+          </Table.Row>
+          </Table.Body>)
+        }
+        
+      </Table>
+    </div>
   )
 }
 
